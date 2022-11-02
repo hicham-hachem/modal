c5dc744507dc44c6d500cd3f8cebfdec03512ad4
hicham <hicham.hachem1@gmail.com>
2022-11-02 23:29:07 +0200

    Open and close modal

---
+++ script.js
@@ -0,0 +1,24 @@
+"use strict";
+
+const modal = document.querySelector(".modal");
+const overlay = document.querySelector(".overlay");
+const btnCloseModel = document.querySelector(".close-modal");
+const btnsOpenModel = document.querySelectorAll(".show-modal");
+
+const openModal = function () {
+    console.log("button clicked");
+    modal.classList.remove("hidden");
+    overlay.classList.remove("hidden");
+};
+
+const closeModal = function () {
+    modal.classList.add("hidden");
+    overlay.classList.add("hidden");
+};
+
+for (let i = 0; i < btnsOpenModel.length; i++) {
+    btnsOpenModel[i].addEventListener("click", openModal);
+}
+
+btnCloseModel.addEventListener("click", closeModal);
+overlay.addEventListener("click", closeModal);
